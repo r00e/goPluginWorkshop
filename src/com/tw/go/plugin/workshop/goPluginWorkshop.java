9f32fa358b17e73a0e4d874cf216ed599950f693
r00e <young_rui@hotmail.com>
2015-05-12 17:33:33 +0800

    implement configuration request; add task.html

--- src/com/tw/go/plugin/workshop/goPluginWorkshop.java
+++ src/com/tw/go/plugin/workshop/goPluginWorkshop.java
@@ -1,5 +1,6 @@
 package com.tw.go.plugin.workshop;
 
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.GoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
@@ -7,6 +8,9 @@ import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.util.Arrays.asList;
 
@@ -50,6 +54,28 @@ public class goPluginWorkshop implements GoPlugin{
     }
 
     private GoPluginApiResponse handleGetConfigRequest() {
-        return null;
+        final HashMap<String, Object> response = new HashMap<String, Object>();
+        HashMap<String, Object> fieldProperty = new HashMap<String, Object>();
+        fieldProperty.put("default-value", null);
+        fieldProperty.put("required", true);
+
+        response.put("script", fieldProperty);
+
+        return new GoPluginApiResponse() {
+            @Override
+            public int responseCode() {
+                return 200;
+            }
+
+            @Override
+            public Map<String, String> responseHeaders() {
+                return null;
+            }
+
+            @Override
+            public String responseBody() {
+                return response == null ? null : new GsonBuilder().create().toJson(response);
+            }
+        };
     }
 }
