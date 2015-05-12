2e9de50c47a9f2b0b06068717a3fb54954859c0c
r00e <young_rui@hotmail.com>
2015-05-12 18:06:10 +0800

    execute

--- src/com/tw/go/plugin/workshop/goPluginWorkshop.java
+++ src/com/tw/go/plugin/workshop/goPluginWorkshop.java
@@ -8,10 +8,15 @@ import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.task.JobConsoleLogger;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import static java.util.Arrays.asList;
 
@@ -43,7 +48,85 @@ public class goPluginWorkshop implements GoPlugin{
     }
 
     private GoPluginApiResponse handleExecution(GoPluginApiRequest request) {
-        return null;
+        HashMap<String, Object> response = new HashMap<String, Object>();
+        String workingDirectory, scriptFileName;
+        Boolean isWindows = isWindows();
+
+        try{
+            Map<String, Object> body = (Map<String, Object>) new GsonBuilder().create().fromJson(request.requestBody
+                    (), Object.class);
+            Map<String, Object> config = (Map<String, Object>) body.get("config");
+            Map<String, String> script = (Map<String, String>) config.get("script");
+            String scriptValue = script.get("value");
+
+            Map<String, Object> context = (Map<String, Object>) body.get("context");
+            Map<String, String> environmentVariables = (Map<String, String>) context.get("environmentVariables");
+            workingDirectory = (String) context.get("workingDirectory");
+            scriptFileName = generateScriptFileName(isWindows);
+
+            createScript(workingDirectory, scriptFileName, isWindows, scriptValue);
+
+            int exitCode = executeScript(workingDirectory, scriptFileName, isWindows, environmentVariables);
+
+            if(exitCode == 0){
+                response.put("success", true);
+                response.put("message", "[pulgin workshop] Script executed successfully.");
+            }else{
+                response.put("success", false);
+                response.put("message", "[pulgin workshop] Script exited with exit code: " + exitCode);
+            }
+        }catch (Exception e){
+            response.put("success", false);
+            response.put("message", "[pulgin workshop] Error: " + e.getMessage());
+        }
+
+        return createResponse(200, response);
+    }
+
+    private void createScript(String workingDirectory, String scriptFileName, Boolean isWindows, String scriptValue) throws IOException, InterruptedException {
+        File file = new File(workingDirectory + "/" + scriptFileName);
+        scriptValue = scriptValue.replaceAll("(\\r\\n|\\r|\\n)", System.getProperty("line.separator"));
+        FileUtils.writeStringToFile(file, scriptValue);
+
+        if(!isWindows){
+            executeCommand(workingDirectory, null, "chmod", "u+x", scriptFileName);
+        }
+
+        JobConsoleLogger.getConsoleLogger().printLine("[plugin workshop] Script written into '" + file.getAbsolutePath
+                () + "'.");
+    }
+
+    private int executeScript(String workingDirectory, String scriptFileName, Boolean isWindows, Map<String, String> environmentVariables) throws IOException, InterruptedException {
+        if (isWindows) {
+            return executeCommand(workingDirectory, environmentVariables, "cmd", "/c", scriptFileName);
+        }
+        return executeCommand(workingDirectory, environmentVariables, "/bin/sh", "-c", "./" + scriptFileName);
+    }
+
+    private int executeCommand(String workingDirectory, Map<String, String> environmentVariables, String... command) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.directory(new File(workingDirectory));
+        if (environmentVariables != null && !environmentVariables.isEmpty()) {
+            processBuilder.environment().putAll(environmentVariables);
+        }
+        Process process = processBuilder.start();
+
+        JobConsoleLogger.getConsoleLogger().readOutputOf(process.getInputStream());
+        JobConsoleLogger.getConsoleLogger().readErrorOf(process.getErrorStream());
+
+        return process.waitFor();
+    }
+
+    private String generateScriptFileName(Boolean isWindows) {
+        return UUID.randomUUID() + (isWindows ? ".bat" : ".sh");
+    }
+
+    private Boolean isWindows() {
+        String osName = System.getProperty("os.name");
+        boolean isWindows = osName.toLowerCase().contains("windows");
+        JobConsoleLogger.getConsoleLogger().printLine("[plugin workshop] OS detected: '" + osName + "'. Is windows? "
+                                                              + isWindows);
+        return isWindows;
     }
 
     private GoPluginApiResponse handleView() {
